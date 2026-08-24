322742fb1d3683b6cbc482f2e6f5a4bf690d6780
Demon :) <siuotp@gmail.com>
2026-08-24 17:26:43 +0700

    :D

--- src/project_fastapi/app/db/__init__.py
+++ src/project_fastapi/app/db/__init__.py
@@ -1,4 +1,4 @@
-from .database import DataBase, create_all, Base
+from .database import DataBase, create_all, Base, connect_db
 from .seed import seed_data
 
-__all__ = ["DataBase", "create_all", "Base", "seed_data"]
+__all__ = ["DataBase", "create_all", "Base", "seed_data", "connect_db"]
